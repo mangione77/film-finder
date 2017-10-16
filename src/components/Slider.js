132609ce323ee1b7624d3d90d6d504be18df6f08
Francesc Edo <francesc.edo.trias@gmail.com>
2017-10-16 17:16:29 +0200

    errors solved v2

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Slider, { Range } from 'rc-slider';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Slider, { Range } from 'rc-slider'
 // We can just import Slider or Range to reduce bundle size
-// import Slider from 'rc-slider/lib/Slider';
-// import Range from 'rc-slider/lib/Range';
-import 'rc-slider/assets/index.css';
+// import Slider from 'rc-slider/lib/Slider'
+// import Range from 'rc-slider/lib/Range'
+import 'rc-slider/assets/index.css'
 
 const Sliderbar = function() {
   return(
