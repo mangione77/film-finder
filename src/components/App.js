cad9be88b3a0b12fd3f4439208f6324e4048012d
Toni Bardina <tonibardina@gmail.com>
2017-10-17 12:44:01 +0200

    Sliderbar almost done

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,26 @@ import React, { Component } from 'react'
 import {Sliderbar} from './Sliderbar'
 import People from './People'
 import Age from './Age'
+import {ResetButton} from './ResetButton'
 
 class App extends Component {
   constructor () {
     super()
+
+    const newDate = new Date()
+    this.currentYear = newDate.getFullYear()  
     this.state = {
-      people: []
+      people: [],
+      genres: [],
+      age: 0,
+      date: [1895, this.currentYear]
     }
     this.getValuePeople = this.getValuePeople.bind(this)
+    this.getAgeValue = this.getAgeValue.bind(this)
+    this.getReset = this.getReset.bind(this)
+    this.getValueSliderBar = this.getValueSliderBar.bind(this)
   }
+
   getValuePeople (value) {
     this.setState({
       people: [...this.state.people, value]
@@ -18,11 +29,40 @@ class App extends Component {
     console.log(this.state)
   }
 
+  getValueSliderBar (value) {
+    this.setState({
+      date: value
+    })
+  }
+
+  getAgeValue (value) {
+    this.setState({
+      age: value
+    })
+  }
+
+  getReset () {
+    this.setState({
+      people: [],
+      genres: [],
+      age: 21,
+      date: [1895, this.currentYear]
+    })
+    console.log(this.state)
+  }
+
+  componentDidUpdate () {
+    console.log(this.state.date)
+
+  }
+
   render () {
     return (
       <div>
-      <People onSubmit={this.getValuePeople} people={this.state.people} />
-        <Sliderbar />
+        <People onSubmit={this.getValuePeople} people={this.state.people} />
+        <Age age={this.state.age} onAgeClick={this.getAgeValue} />
+        <Sliderbar dataValue={this.state.date} currentValue={this.getValueSliderBar} defaultValue={[1895, this.currentYear]} />
+        <ResetButton foo={this.getReset} />
       </div>
     )
   }
