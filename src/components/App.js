f158c2a00c213a26994f0e8925185689d1310550
Francesc Edo <francesc.edo.trias@gmail.com>
2017-10-17 12:57:07 +0200

    merge

--- src/components/App.js
+++ src/components/App.js
@@ -2,41 +2,29 @@ import React, { Component } from 'react'
 import {Sliderbar} from './Sliderbar'
 import People from './People'
 import Age from './Age'
-<<<<<<< HEAD
 import Genres from './Genres'
-
-=======
 import {ResetButton} from './ResetButton'
 import {sendData} from './api'
->>>>>>> fa616e0f2ef057c6c8bcef7168644741850562c9
 
 class App extends Component {
   constructor () {
     super()
 
     const newDate = new Date()
-    this.currentYear = newDate.getFullYear()  
+    this.currentYear = newDate.getFullYear()
     this.state = {
       people: [],
       genres: [],
-<<<<<<< HEAD
-      age: 21,
-      date: {}
-    }
-    this.getValuePeople = this.getValuePeople.bind(this)
-    this.getAgeValue = this.getAgeValue.bind(this)
-    this.getGenresValue = this.getGenresValue.bind(this)
-=======
       age: 0,
       date: [1895, this.currentYear],
       ajaxData: []
     }
     this.getValuePeople = this.getValuePeople.bind(this)
+    this.getGenresValue = this.getGenresValue.bind(this)
     this.getAgeValue = this.getAgeValue.bind(this)
     this.getReset = this.getReset.bind(this)
     this.getValueSliderBar = this.getValueSliderBar.bind(this)
     this.deleteValuePeople = this.deleteValuePeople.bind(this)
->>>>>>> fa616e0f2ef057c6c8bcef7168644741850562c9
   }
   /* GET FILTER VALUES */
   getValuePeople (value) {
@@ -58,8 +46,20 @@ class App extends Component {
       people: newArray
     })
   }
-  componentDidUpdate () {
-    console.log(this.state.age)
+
+  getGenresValue (value) {
+    this.setState({
+      genres: value
+    })
+  }
+  getReset () {
+    this.setState({
+      people: [],
+      genres: [],
+      age: 21,
+      date: [1895, this.currentYear]
+    })
+    console.log(this.state)
   }
 
   /* GET DATA AJAX */
@@ -71,7 +71,6 @@ class App extends Component {
       })
     })
   }
-  
   /* COMPONENT METHODS */
   componentWillMount () {
     this.getAjaxData()
@@ -83,43 +82,14 @@ class App extends Component {
     })
   }
 
-  getAgeValue (value) {
-    this.setState({
-      age: value
-    })
-  }
-
-<<<<<<< HEAD
-  getGenresValue(value) {
-    this.setState({
-      genres:value
-    })
-  }
-
-=======
-  getReset () {
-    this.setState({
-      people: [],
-      genres: [],
-      age: 21,
-      date: [1895, this.currentYear]
-    })
-    console.log(this.state)
-  }
-
   componentDidUpdate () {
+    console.log(this.state.age)
     console.log(this.state.date)
-
   }
 
->>>>>>> fa616e0f2ef057c6c8bcef7168644741850562c9
   render () {
     return (
       <div>
-        <People onSubmit={this.getValuePeople} people={this.state.people} />
-        <Age age={this.state.age} onAgeClick={this.getAgeValue} />
-        <Sliderbar dataValue={this.state.date} currentValue={this.getValueSliderBar} defaultValue={[1895, this.currentYear]} />
-        <ResetButton foo={this.getReset} />
         <People
           onSubmit={this.getValuePeople}
           people={this.state.people}
