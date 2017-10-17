d47fe239392b176aa0707bba6cd5271d171a2132
Pablo Mangione <mangione.mkt@gmail.com>
2017-10-17 12:48:26 +0200

    added Genres component and edited App

--- src/components/App.js
+++ src/components/App.js
@@ -4,31 +4,25 @@ import People from './People'
 import Age from './Age'
 import Genres from './Genres'
 
+
 class App extends Component {
   constructor () {
     super()
     this.state = {
       people: [],
-      genres: {
-        action:false,
-        adventure:false,
-        comedy:false,
-        drama:false,
-        horror:false,
-        science_fiction:false
-      },
-      age: 0,
+      genres: [],
+      age: 21,
       date: {}
     }
     this.getValuePeople = this.getValuePeople.bind(this)
     this.getAgeValue = this.getAgeValue.bind(this)
+    this.getGenresValue = this.getGenresValue.bind(this)
   }
 
   getValuePeople (value) {
     this.setState({
       people: [...this.state.people, value]
     })
-    console.log(this.state)
   }
 
   getAgeValue (value) {
@@ -37,18 +31,19 @@ class App extends Component {
     })
   }
 
-  componentDidUpdate() {
-    console.log(this.state.age)
+  getGenresValue(value) {
+    this.setState({
+      genres:value
+    })
   }
 
-
-  
   render () {
     return (
       <div>
       <People onSubmit={this.getValuePeople} people={this.state.people} />
       <Age age={this.state.age} onAgeClick={this.getAgeValue} />
         <Sliderbar />
+      <Genres onGenreClick={this.getGenresValue} />
       </div>
     )
   }
--- src/components/Genres.js
+++ src/components/Genres.js
@@ -1,2 +1,51 @@
 import React, { Component } from 'react'
-import { ToggleButtonGroup, ToggleButtom} from 'react-bootstrap'
+import { ButtonGroup, Button } from 'react-bootstrap'
+
+
+class Genres extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			genres: []
+		}
+
+		this.onGenreClickHandler = this.onGenreClickHandler.bind(this)
+		this.onGenreSubmit = this.onGenreSubmit.bind(this)
+	}
+	
+	onGenreClickHandler = (event) => { 
+		event.preventDefault()	
+		if (this.state.genres.includes(event.target.name) === false) {
+		this.setState({
+			genres:this.state.genres.concat(event.target.name) // sets component state to the name of the genre button clicked
+		})
+
+		this.props.onGenreClick(this.state.genres) // concats the genres to the app main state
+	}
+		
+	}
+
+	onGenreSubmit = (event) => {
+		event.preventDefault()
+		this.setState({
+			genres:[] // empties already selected genres
+		})
+		this.props.onGenreClick(this.state.genres) // empties main component genres state
+	}
+
+
+	render() {
+		return (<ButtonGroup>
+				<Button name="action" onClick={this.onGenreClickHandler}>Action</Button>
+				<Button name="adventure" onClick={this.onGenreClickHandler}>Adventure</Button>
+				<Button name="comedy" onClick={this.onGenreClickHandler}>Comedy</Button>
+				<Button name="drama" onClick={this.onGenreClickHandler}>Drama</Button>
+				<Button name="horror" onClick={this.onGenreClickHandler}>Horror</Button>
+				<Button name="science_fiction" onClick={this.onGenreClickHandler}>Sci-Fi</Button>
+				<Button name="confirm" onClick={this.onGenreSubmit}>Clear!</Button>
+				</ButtonGroup>)
+	}
+}
+
+export default Genres
