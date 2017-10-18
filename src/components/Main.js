9fe6708bb2fef302d72a7629dc009a40e64f75db
Pablo Mangione <mangione.mkt@gmail.com>
2017-10-18 11:29:09 +0200

    merge

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,15 @@
 import React from 'react'
-import { Grid, Row, Col, Thumbnail, Button } from 'react-bootstrap'
 
 export const Main = (props) => (
 	<div>
 
-	<Grid>
-    <Row>
-    <Col xs={6} md={4}>
+
     {
       props.allMovies.map((movie) => {
-        return <Thumbnail src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}>
-              </Thumbnail>
+        return <img className="images" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}>
+              </img>   
       })
      } 
-    </Col>
-  </Row>
-</Grid>      
+     
 	</div>
 )
