1103572371bdea594854dc12d0fc9b4eb83de257
Toni Bardina <tonibardina@gmail.com>
2017-10-18 13:48:00 +0200

    Hover add

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
+
 export const Main = (props) => (
 	<div id="image-container">
     {
       props.allMovies.map((movie) => {
-        return <img className= "images" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}>
-               </img>
-                //todo hover
-      })
-     }     
-	</div>
+        return ( 
+            <div class="page-post">
+              <h5 className="movie-title">{movie.title}<br />&#9734;{movie.vote_average}</h5>
+              <p className="movie-description">{ movie.overview.substring(0,50) + '...' }</p>
+              <img className= "images" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+            </div>
+        )
+     })
+    }
+  </div>        
 )
