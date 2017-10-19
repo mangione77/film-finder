6c0030654d21d35b94620bafff2fb08fa5e05940
Francesc Edo <francesc.edo.trias@gmail.com>
2017-10-19 12:08:08 +0200

    changes before commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,7 +14,6 @@ class Main extends Component {
         this.last = element.scrollTop
         this.props.loadMore()
       }
-
     }
     // console.log(element.scrollTop, elementHeight)
   }
@@ -30,32 +29,26 @@ class Main extends Component {
       <div id='image-container'>
         {
           this.props.allMovies.map((movie, id) => {
-        return (
-        
-        <div className="page-post-button page-post">
-
-          <a className="page-post-button page-post" href={'#popup-' + id}>
-          <div key={id}>
-            <h5 className='movie-title'>{movie.title}<br />&#9734;{movie.vote_average}</h5>
-            <p className='movie-description'>{ movie.overview.substring(0, 50) + '...' }</p>
-            <img className='images' src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-          </div>
-          </a>
-
-          <div className="overlay popup-1" id={'popup-' + id}>
-            <div className="popup">
-              <div className="popup-content">
-                <h1>{movie.title}</h1>
-                <p>{movie.overview}</p>
-                <a class="close" href="#">&times;</a>
+            return (
+              <div className='page-post-button page-post'>
+                <a className='page-post-button page-post' href={'#popup-' + id}>
+                  <div key={id}>
+                    <h5 className='movie-title'>{movie.title}<br />&#9734;{movie.vote_average}</h5>
+                    <p className='movie-description'>{ movie.overview.substring(0, 50) + '...' }</p>
+                    <img className='images' src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+                  </div>
+                </a>
+                <div className='overlay popup-1' id={'popup-' + id}>
+                  <div className='popup'>
+                    <div className='popup-content'>
+                      <h1>{movie.title}</h1>
+                      <p>{movie.overview}</p>
+                      <a class='close' href='#'>&times;</a>
+                    </div>
+                  </div>
+                </div>
               </div>
-            </div> 
-          </div> 
-
-        </div>  
-
-              
-        )
+            )
           })
         }
         <button onClick={this.props.loadMore}>Load More results</button>
