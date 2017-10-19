bf62b61bdaccd358774eb26f808e5f5b7387cbde
Francesc Edo <francesc.edo.trias@gmail.com>
2017-10-19 16:04:35 +0200

    toni pesao

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import { Grid, Row, Col } from 'react-bootstrap'
+// import { Grid, Row, Col } from 'react-bootstrap'
 import React from 'react'
 import Genres from './Genres'
 import People from './People'
@@ -10,30 +10,42 @@ import './css/Sidebar.css'
 
 export const Sidebar = (props) => {
   return (
-    <div className="fix-sidebar">
+    <div className='fix-sidebar'>
       <div className='logo-container'>
         <img id='logo' src={photoUrl} alt='Logo' />
       </div>
-      <Genres
-        genreAdd={props.genreAdd}
-        genreDelete={props.genreDelete}
-        genreValue={props.genreValue}
-        genreClear={props.genreClear}
-      />
-      <Age
-        ageValue={props.ageValue}
-        onAgeClick={props.onAgeClick}
-      />
-      <People
-        onSubmit={props.onSubmit}
-        people={props.people}
-        onDelete={props.onDelete}
-      />
-      <Sliderbar
-        data={props.data}
-        currentValue={props.currentValue}
-        defaultValue={props.defaultValue}
-      />
+      <div>
+        <h4>Genres</h4>
+        <Genres
+          genreAdd={props.genreAdd}
+          genreDelete={props.genreDelete}
+          genreValue={props.genreValue}
+          genreClear={props.genreClear}
+        />
+      </div>
+      <div className='section'>
+        <h4>Ages</h4>
+        <Age
+          ageValue={props.ageValue}
+          onAgeClick={props.onAgeClick}
+        />
+      </div>
+      <div>
+        <h4>Actor / Director</h4>
+        <People
+          onSubmit={props.onSubmit}
+          people={props.people}
+          onDelete={props.onDelete}
+        />
+      </div>
+      <div>
+        <h4>Release year</h4>
+        <Sliderbar
+          data={props.data}
+          currentValue={props.currentValue}
+          defaultValue={props.defaultValue}
+        />
+      </div>
       <ResetButton
         resetData={props.resetData}
       />
