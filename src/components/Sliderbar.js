47493731a0cdb3e2b8d20115acccaaae42fa218e
Francesc Edo <francesc.edo.trias@gmail.com>
2017-10-20 10:40:11 +0200

    all responsive + menu open close

--- src/components/Sliderbar.js
+++ src/components/Sliderbar.js
@@ -1,21 +1,21 @@
-import 'rc-slider/assets/index.css';
-import 'rc-tooltip/assets/bootstrap.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Tooltip from 'rc-tooltip';
-import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css'
+import 'rc-tooltip/assets/bootstrap.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Tooltip from 'rc-tooltip'
+import Slider from 'rc-slider'
 
-const createSliderWithTooltip = Slider.createSliderWithTooltip;
-const Range = createSliderWithTooltip(Slider.Range);
-const wrapperStyle = { width: 200, margin: 20 };
+const createSliderWithTooltip = Slider.createSliderWithTooltip
+const Range = createSliderWithTooltip(Slider.Range)
+const wrapperStyle = { width: 200, margin: 20 }
 
-const newDate = new Date();
-const currentYear = newDate.getFullYear();
+const newDate = new Date()
+const currentYear = newDate.getFullYear()
 
 export const Sliderbar = props => (
-  <div className="sliderbar-container">
+  <div className='sliderbar-container'>
     <div style={wrapperStyle}>
-      <Range min={1895} max={2017} allowCross={false} onAfterChange= {props.currentValue} defaultValue={[1895, currentYear]} tipFormatter={value => `${value}`} />
+      <Range min={1895} max={2017} allowCross={false} onAfterChange={props.currentValue} defaultValue={[1895, currentYear]} tipFormatter={value => `${value}`} />
     </div>
   </div>
 )
